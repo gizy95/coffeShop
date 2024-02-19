131e07894a6ce74bced0a6dc92ab5f14cd179d98
Antonio Bjelobrk <bjelobrka@gmail.com>
2024-02-19 15:57:37 +0100

    last version

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,23 +1,22 @@
-import  "../styleMathieu/Register.css";
+import "../styleMathieu/Register.css";
 
 export default function Register() {
 
 
-    return (   
+    return (
         <div className="containerRegister">
-                
+
             <div className="registerLeft">
                 <h1 className="registerTitle
                 ">Receive our bloody offers every week</h1>
-            <br />
-            <button className="buttonMathieu">Order Now</button>
+                <br />
+                <button className="buttonMathieu">Order Now</button>
             </div>
             <div className="registerRight">
-                <p className="textOverlayGirl">Buy more stuff</p>
                 <img className="registerGirl" src="src/assets/Pics Coffee/girl.png" alt="" />
             </div>
-          
-         
+
+
         </div>
-      );
-    }
+    );
+}
