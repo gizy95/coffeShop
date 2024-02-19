9759bd7811d1dc3cfe1eac414a61ef6862a2299e
gizy95 <bjelobrka@gmail.com>
2024-02-19 16:06:58 +0100

    Update Order.jsx

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -9,7 +9,7 @@ export default function Order() {
           <h1 className="titleOrder" style={{justifyContent: "center"}}>Now you can also buy our coffee and brew it at home</h1>   
 
 <div className="insideOrder" style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                <img  className="pubOrder" src="./src/assets/Pics Coffee/10�.png" alt="" />
+                <img  className="pubOrder" src="/src/assets/Pics Coffee/10�.png" alt="" />
                 
                 <img className="bagsOrder" src="src/assets/Pics Coffee/bags.png" alt="cofee" />
                 <button className="buttonMathieu">Order Now</button>
