e872c3986965c94bf8590e27fe33007be45196ba
Maiwand Aryan <maiwandaryan@gmail.com>
2024-02-19 15:30:36 +0100

    Fixed links hover

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import React from "react";
-import LandingPage from "./components/aboutus_mai";
 import NavBar from "./components/Navbar";
 import AboutUs from "./components/AboutUs";
 import TastingDay from "./components/TastingDay";
