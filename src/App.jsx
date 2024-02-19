f6d9ca51526c23b9788a61921ac21a4b6d9db244
Antonio Bjelobrk <bjelobrka@gmail.com>
2024-02-19 15:25:31 +0100

    finished the 3 sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
 import React from "react"
+import Menu from './components/Menu'
+import OpenHours from './components/OpenHours'
+import SmilingGuy from './components/SmilingGuy'
 
 function App() {
 
 
   return (
-    <div>
-      <p>SOMETHING</p>
-    </div>
+    <>
+      <Menu />
+      <OpenHours />
+      <SmilingGuy />
+    </>
   )
 }
 
